fcba0fc156d6b1b87d92a65ab7f01d90bb7c0fd3
trity <trity@ganguo.hk>
2016-10-25 01:12:20 +0800

    完善pinduoduo，添加io

--- pinduoduo.py
+++ pinduoduo.py
@@ -2,7 +2,7 @@ import requests
 import json
 from datetime import datetime, timedelta
 
-
+# 输出测试
 def request_url_print(product_id):
     url = "http://apiv2.yangkeduo.com/goods/%s/local_group" % product_id
     r = requests.get(url)
@@ -19,6 +19,14 @@ def request_url_print(product_id):
         for (k, v) in dict_map.items():
             print(k, v)
 
+# 加载对应的商品列表详情
+def request_product(product_id):
+    url = "http://apiv2.yangkeduo.com/v2/goods/%s" % product_id
+    r = requests.get(url)
+    json_raw = r.json()
+    # print(json_raw["goods_name"])
+    return json_raw["goods_name"]
+
 def request_url(product_id):
     url = "http://apiv2.yangkeduo.com/goods/%s/local_group" % product_id
     r = requests.get(url)
@@ -33,25 +41,38 @@ def request_url(product_id):
         time=int(expire_time)-server_time
         now=datetime.now() # 得到当前时间
         future_time=now+timedelta(hours=time/3600)
-        print("product_id:",product_id)
-        print("nickName:",dict_map["nickname"])
-        print(future_time)
+        product_url="http://mobile.yangkeduo.com/goods.html?goods_id=%s" % product_id
+        product_name=request_product(product_id)
+        # print("nickName:",dict_map["nickname"])
+        # print(future_time)
+        return dict(url=product_url,name=product_name,nickName=dict_map["nickname"],finish_time=future_time.strftime('%Y-%m-%d %H:%M:%S'))
 
+# 读取商品列表
 def read_product_id(file_path):
-    file_object = open(file_path) 
-    try: 
-      all_the_text = file_object.readlines() 
-      # print(all_the_text) 
-      return all_the_text
-    finally: 
-      file_object.close() 
+    with open(file_path) as file_object:
+        return file_object.readlines()
+
+def write_product_spell_group(info_list):
+    # print(info_list)
+    product_info_str=""
+    with open("./product_spell_group.txt","w+") as f:
+        # f.write(json.dumps(info_list)) # 转换成json
+        for index in range(0,len(info_list)):
+            f.write(info_list[index]+"\n")
+
 
 # 主程序调用执行
 
-# 没有填完所有的商品
 product_list=read_product_id("./product_id.txt")
-# print(len(product_list))
+product_group=[]
 for index in range(0,len(product_list)):
-    # print(product_list[index].rstrip("\n"))
-    request_url(product_list[index].rstrip("\n"))
+    tmp=request_url(product_list[index].strip())
+    if tmp:
+        for(k,v) in tmp.items():
+            tmp_test=str(k)+":"+str(v.strip('\ue035').strip('\U0001f48b').strip('\U0001f495'))
+            product_group.append(tmp_test)
+        product_group.append("\n")
+    
+    if len(product_group):
+        write_product_spell_group(product_group)
 
