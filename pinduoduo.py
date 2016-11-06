14a9780c4b20865e8ec15dd28bee11c2735e3d2f
trity <trity@ganguo.hk>
2016-11-07 00:13:52 +0800

    修复正则表达式过滤无法显示的特殊字符

--- pinduoduo.py
+++ pinduoduo.py
@@ -33,7 +33,6 @@ def request_product(product_id):
     url = "http://apiv2.yangkeduo.com/v2/goods/%s" % product_id
     r = requests.get(url)
     json_raw = r.json()
-    # print(json_raw["goods_name"])
     return json_raw["goods_name"]
 
 
@@ -46,10 +45,11 @@ def request_url(product_id):
     json_raw = r.json()
     server_time = json_raw['server_time']
     length = len(json_raw["local_group"])
-    # print(length)  # 当有多个开团的情况有用。
     for index in range(0, length):
         dict_str = json_raw["local_group"][index]
-        dict_map = json.loads(dict_str)
+        dict_str_filter = re.sub(
+            r'\b\\U\d{3}[a-f0-9]{5}|\b\\ue\w{3}', '', dict_str)# 过滤昵称使用特殊符号无法显示的情况。
+        dict_map = json.loads(dict_str_filter)
         expire_time = dict_map["expire_time"]
         time = int(expire_time) - server_time
         now = datetime.now()  # 得到当前时间
@@ -59,9 +59,6 @@ def request_url(product_id):
 
         product_list.append(dict(url=product_url, name=product_name, nickName=dict_map[
                             "nickname"], finish_time=future_time.strftime('%Y-%m-%d %H:%M:%S')))
-        # return
-        # dict(url=product_url,name=product_name,nickName=dict_map["nickname"],finish_time=future_time.strftime('%Y-%m-%d
-        # %H:%M:%S'))
     return product_list
 
 # 读取商品列表
@@ -73,10 +70,7 @@ def read_product_id(file_path):
 
 
 def write_product_spell_group(info_list):
-    # print(info_list)
-    product_info_str = ""
     with open("./product_spell_group.txt", "w+") as f:
-        # f.write(json.dumps(info_list)) # 转换成json
         for index in range(0, len(info_list)):
             f.write(info_list[index] + "\n")
 
@@ -91,8 +85,7 @@ for index in range(0, len(product_list)):
         for index_tmp in range(0, len(tmp)):
             if tmp[index_tmp]:
                 for(k, v) in tmp[index_tmp].items():
-                    tmp_data = str(k) + ":" + str(re.sub(r'\b\\U\d{3}[a-f0-9]{5}','',re.sub(r'\b\\ue\d{3}','',v)))
-                    product_group.append(tmp_data)
+                    product_group.append(v)
                 product_group.append("\n")
 
 if len(product_group):
