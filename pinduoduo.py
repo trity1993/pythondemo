b4852a7e7fdb07b510ab3e2698f89d9b76037cb5
trity <trity@ganguo.hk>
2016-12-31 17:01:12 +0800

    开团按时间进行排序，加入显示对应的价格

--- pinduoduo.py
+++ pinduoduo.py
@@ -29,25 +29,32 @@ def request_url_print(product_id):
 # 加载对应的商品列表详情
 
 
-def request_product(product_id):
+def request_product(product_id, goods_item_price=0):
     url = "http://apiv2.yangkeduo.com/v2/goods/%s" % product_id
     r = requests.get(url)
     json_raw = r.json()
-    return json_raw["goods_name"]
+
+    group_product_item = json_raw["group"]
+    for x in range(0, len(group_product_item)):
+        if group_product_item[x]["customer_num"] == 2:
+            goods_item_price = group_product_item[x]["price"]
+
+    return json_raw["goods_name"], goods_item_price  # 返回商品名称和价格
 
 
 def request_url(product_id):
     if len(product_id) == 0:
         return None
-    product_list = []
+    product_list = []  # 存储多个开团的情况
     url = "http://apiv2.yangkeduo.com/goods/%s/local_group" % product_id
     r = requests.get(url)
     json_raw = r.json()
-    server_time = json_raw['server_time'] # 得到服务器时间
+    server_time = json_raw['server_time']  # 得到服务器时间
     length = len(json_raw["local_group"])
     for index in range(0, length):
         dict_str = json_raw["local_group"][index]
-        dict_str_filter = re.sub(r'\\U\d{3}[a-f0-9]{5}|\\ue\w{3}', '', dict_str)# 过滤昵称使用特殊符号无法显示的情况。
+        dict_str_filter = re.sub(
+            r'\\U\d{3}[a-f0-9]{5}|\\ue\w{3}', '', dict_str)  # 过滤昵称使用特殊符号无法显示的情况。
         dict_map = json.loads(dict_str_filter)
         # 计算对应的时间
         expire_time = dict_map["expire_time"]
@@ -55,10 +62,14 @@ def request_url(product_id):
         now = datetime.now()  # 得到当前时间
         future_time = now + timedelta(hours=time / 3600)
         product_url = "http://mobile.yangkeduo.com/goods.html?goods_id=%s" % product_id
-        product_name = request_product(product_id)
+        product_name, product_price = request_product(product_id)
 
-        product_list.append(dict(url=product_url, name=product_name, nickName=dict_map[
-                            "nickname"], finish_time=future_time.strftime('%Y-%m-%d %H:%M:%S')))
+        # print("价格为" + str(product_price / 100))
+        product_info = product_url + "\r\n" + product_name + "\r\n" + dict_map["nickname"] + "\r\n" + str(
+            product_price / 100) + "\r\n" + future_time.strftime('%Y-%m-%d %H:%M:%S') + "\r\n"
+
+        product_list.append(dict(product_info=product_info,
+                                 time_stamp=time))
     return product_list
 
 # 读取商品列表
@@ -70,24 +81,26 @@ def read_product_id(file_path):
 
 
 def write_product_spell_group(info_list):
-    with open("./product_pinduoduo_spell_group.txt", "w+",encoding='utf-8') as f:
+    with open("./product_pinduoduo_spell_group.txt", "w+", encoding='utf-8') as f:
         for index in range(0, len(info_list)):
-            f.write(info_list[index] + "\n")
+            f.write(info_list[index]["product_info"] + "\n")
 
 
 # 主程序调用执行
 
 product_list = read_product_id("./product_pinduoduo_id.txt")
 product_group = []
+product_str = ""
 for index in range(0, len(product_list)):
     tmp = request_url(product_list[index].strip())
     if tmp:
         for index_tmp in range(0, len(tmp)):
             if tmp[index_tmp]:
-                for(k, v) in tmp[index_tmp].items():
-                    product_group.append(v)
-                product_group.append("\n")
+                product_group.append(tmp[index_tmp])
 
 if len(product_group):
-    print(product_group)
-    write_product_spell_group(product_group)
+    L=sorted(product_group, key=lambda s: s["time_stamp"])
+    print(L)
+    write_product_spell_group(L)
+else:
+    print(product_group.append("暂时无任何的开团情况"))
